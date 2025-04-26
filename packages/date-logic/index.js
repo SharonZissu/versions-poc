b522b3d02771b36c9ad1663c61c0cf37a0e57e7e
sharonzissu <sharonzissu10@gmail.com>
2025-04-26 18:40:22 +0300

    feat(data-logic): check3

--- packages/date-logic/index.js
+++ packages/date-logic/index.js
@@ -1,13 +1,13 @@
 module.exports = function getDate(
     locale = 'en-US',
-    options = { weekday: 'longg', year: 'numeric', month: 'long', day: 'numeric' }
+    options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }
 ) {
     return new Date().toLocaleDateString(locale, options);
 };
 
 module.exports = function getDate2(
     locale = 'en-US',
-    options = { weekday: 'longg', year: 'numeric', month: 'long', day: 'numeric' }
+    options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }
 ) {
     return new Date().toLocaleDateString(locale, options);
 };
