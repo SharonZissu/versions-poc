b26bb2845ac60bccb92225cf03957952f26550dd
sharonzissu <sharonzissu10@gmail.com>
2025-04-30 17:26:07 +0300

    feat: change

--- check.js
+++ check.js
@@ -1,6 +1,5 @@
-const createPreset = require('conventional-changelog-conventionalcommits');
-
-module.exports = createPreset({
-    issuePrefixes: ['TEST-'],
-    issueUrlFormat: 'https://myBugTracker.com/{{prefix}}{{id}}'
-});
+module.exports = import( 'conventional-changelog-conventionalcommits')
+    .then( pkg => pkg.default({
+        issuePrefixes: ['TEST-'],
+        issueUrlFormat: 'https://myBugTracker.com/{{prefix}}{{id}}'
+    }));
